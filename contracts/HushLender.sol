bde15bdcb69a35f6fc2cb413f3784ca4227b0ae1
LHerskind <lasse.herskind@gmail.com>
2021-01-19 15:57:19 +0100

    Partially implemented flashloan
    Added some section of the flashloan, but ran into issues with 1inch API.

--- contracts/HushLender.sol
+++ contracts/HushLender.sol
@@ -2,27 +2,33 @@
 pragma solidity 0.6.12;
 
 import "hardhat/console.sol";
-import {IERC20, ILendingPool, IProtocolDataProvider} from "./Interfaces.sol";
-import {SafeERC20} from "./Libraries.sol";
+import {
+    IERC20,
+    ILendingPool,
+    IProtocolDataProvider,
+    ILendingPoolAddressesProvider,
+    IFlashLoanReceiver
+} from "./Interfaces.sol";
+import {SafeERC20, SafeMath} from "./Libraries.sol";
 
 interface IHushLender {
-    function deposit(address _asset, uint256 _amount) external;
+    function deposit(address _asset, uint256 _amountBorrowed) external;
 
     function withdraw(address _asset) external;
 
-    function borrow(address _asset, uint256 _amount) external;
+    function borrow(address _asset, uint256 _amountBorrowed) external;
 
-    function repay(address _asset, uint256 _amount) external;
+    function repay(address _asset, uint256 _amountBorrowed) external;
 
     function transferAsset(
         address _asset,
-        uint256 _amount,
+        uint256 _amountBorrowed,
         address _to
     ) external;
 
     function transferAToken(
         address _asset,
-        uint256 _amount,
+        uint256 _amountBorrowed,
         address _to
     ) external;
 
@@ -38,8 +44,9 @@ interface IHushLender {
         );
 }
 
-contract HushLender is IHushLender {
+contract HushLender is IHushLender, IFlashLoanReceiver {
     using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
     address public owner;
 
@@ -63,15 +70,15 @@ contract HushLender is IHushLender {
      * @notice Deposits an `amount` of `_asset` into the underlying Aave reserve and thereby receiving the overlying interestbearing aToken,
      * e.g., user deposits 1 WETH and receives 1 aWETH.
      * @param _asset The address of the underlying asset
-     * @param _amount The amount to be deposited
+     * @param _amountBorrowed The amount to be deposited
      */
-    function deposit(address _asset, uint256 _amount)
+    function deposit(address _asset, uint256 _amountBorrowed)
         public
         override
         onlyOwner
     {
-        IERC20(_asset).safeApprove(address(lendingPool), _amount);
-        lendingPool.deposit(_asset, _amount, address(this), 0);
+        IERC20(_asset).safeApprove(address(lendingPool), _amountBorrowed);
+        lendingPool.deposit(_asset, _amountBorrowed, address(this), 0);
     }
 
     /**
@@ -83,55 +90,63 @@ contract HushLender is IHushLender {
     }
 
     /**
-     * @notice Borrow a specific `_amount` of the underlying `_asset` if the user has sufficient collateral
+     * @notice Borrow a specific `_amountBorrowed` of the underlying `_asset` if the user has sufficient collateral
      * will borrow with stable interest rate and 0 as the referral code.
      * @param _asset The address of the underlying asset to borrow
-     * @param _amount The amount to be borrowed
+     * @param _amountBorrowed The amount to be borrowed
      */
-    function borrow(address _asset, uint256 _amount) public override onlyOwner {
-        lendingPool.borrow(_asset, _amount, 1, 0, address(this));
+    function borrow(address _asset, uint256 _amountBorrowed)
+        public
+        override
+        onlyOwner
+    {
+        lendingPool.borrow(_asset, _amountBorrowed, 1, 0, address(this));
     }
 
     /**
-     * @notice Repays a borrowed `_amount` on a specific reserve, burning the equivalent debt tokens.
+     * @notice Repays a borrowed `_amountBorrowed` on a specific reserve, burning the equivalent debt tokens.
      * will repay with stable interest rate.
      * @param _asset The address of the borrowed underlying asset previously borrowed
-     * @param _amount The amount to pay back.
+     * @param _amountBorrowed The amount to pay back.
      */
-    function repay(address _asset, uint256 _amount) public override onlyOwner {
-        IERC20(_asset).safeApprove(address(lendingPool), _amount);
-        lendingPool.repay(_asset, _amount, 1, address(this));
+    function repay(address _asset, uint256 _amountBorrowed)
+        public
+        override
+        onlyOwner
+    {
+        IERC20(_asset).safeApprove(address(lendingPool), _amountBorrowed);
+        lendingPool.repay(_asset, _amountBorrowed, 1, address(this));
     }
 
     /**
      * @notice Transfers a given `amount` of the `asset` to another account.
      * @param _asset The address of the asset
-     * @param _amount The amount to transfer
+     * @param _amountBorrowed The amount to transfer
      * @param _to The address to receive the asset
      */
     function transferAsset(
         address _asset,
-        uint256 _amount,
+        uint256 _amountBorrowed,
         address _to
     ) public override onlyOwner {
-        IERC20(_asset).safeTransfer(_to, _amount);
+        IERC20(_asset).safeTransfer(_to, _amountBorrowed);
     }
 
     /**
      * @notice Transfers a given `amount` of the interest bearing `asset` to another account.
      * Used as a helper function but `transferAsset` could be used with the aTokenAddress directly.
      * @param _asset The address of the underlying asset
-     * @param _amount The amount of aTokens to Transfer
+     * @param _amountBorrowed The amount of aTokens to Transfer
      * @param _to The address to receive the asset
      */
     function transferAToken(
         address _asset,
-        uint256 _amount,
+        uint256 _amountBorrowed,
         address _to
     ) public override onlyOwner {
         (address aTokenAddress, , ) =
             dataProvider.getReserveTokensAddresses(_asset);
-        IERC20(aTokenAddress).safeTransfer(_to, _amount);
+        IERC20(aTokenAddress).safeTransfer(_to, _amountBorrowed);
     }
 
     /**
@@ -175,5 +190,119 @@ contract HushLender is IHushLender {
         variableDebt = IERC20(variableDebtTokenAddress).balanceOf(
             address(this)
         );
+    }
+
+    ////////////////
+    // Flashloans //
+    ////////////////
+
+    /**
+    * @notice Initiates a flashloan of `_amountBorrowed` of `_asset` that is to be swapped at 1inch with `_inchdata`. 
+    * @param _asset The asset we want to borrow
+    * @param _amountBorrowed The amount of the asset we want to borrow
+    * @param _inchdata calldata used for the swap. 
+     */
+    function takeFlashloan(
+        address _asset,
+        uint256 _amountBorrowed,
+        bytes calldata _inchdata
+    ) public {
+        address[] memory assets = new address[](1);
+        assets[0] = _asset;
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = _amountBorrowed;
+
+        // 0 = no debt, 1 = stable, 2 = variable
+        uint256[] memory modes = new uint256[](1);
+        modes[0] = 0;
+
+        //bytes memory params = "";
+        //uint16 referralCode = 0;
+
+        // 1. Borrow `_amountBorrowed` of `_asset`
+        lendingPool.flashLoan(
+            address(this), // receiverAddress
+            assets,
+            amounts,
+            modes,
+            address(this), // onBehalfOf
+            _inchdata, //params
+            0 // referralcode
+        );
+    }
+
+    /**
+     * @notice This will be called as part of the flashloan
+     * @param assets The addresses of the borrowed assets
+     * @param amounts The amounts of the borrowed assets
+     * @param premiums The fee that we must pay to borrow
+     * @param initiator The initiator of the flashswap
+     * @param params The params for our function (here 1inch calldata)
+     * @return True if executed
+     */
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        // I have funds here. Time to do nice stuff!
+        if (false) {
+            initiator;
+        }
+
+        // Superlong ETH! Here we have only 1 asset.
+        uint256 owns = amounts[0].add(premiums[0]);
+        _superLongEth(assets[0], amounts[0], owns, params);
+
+        // 5. Pay back flashloan
+        IERC20(assets[0]).safeApprove(address(lendingPool), owns);
+
+        return true;
+    }
+
+    /**
+     * @notice Receives `_amountBorrowed` of `_asset` which it swaps to weth.
+     * Then deposits weth into Aave and borrows `_amountPayback` of `_asset` to pay back loan.
+     * @param _asset The asset that it receives
+     * @param _amountBorrowed The amount it receives
+     * @param _amountPayback The amount to pay back
+     * @param _inchdata The 1inch swap data
+     */
+    function _superLongEth(
+        address _asset,
+        uint256 _amountBorrowed,
+        uint256 _amountPayback,
+        bytes calldata _inchdata
+    ) internal {
+        address inch = address(0x111111125434b319222CdBf8C261674aDB56F3ae);
+        IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+
+        // 2. Swap _asset to WETH
+        IERC20(_asset).safeApprove(inch, _amountBorrowed);
+        (bool success, ) = inch.call(_inchdata);
+        require(success, "1inch call failed");
+
+        uint256 wethBalance = weth.balanceOf(address(this));
+        // 3. Deposit all weth into Aave
+        deposit(address(weth), wethBalance);
+
+        // 4. Borrow enough _asset to buy back
+        borrow(_asset, _amountPayback);
+    }
+
+    function LENDING_POOL() public view override returns (ILendingPool) {
+        return lendingPool;
+    }
+
+    function ADDRESSES_PROVIDER()
+        public
+        view
+        override
+        returns (ILendingPoolAddressesProvider)
+    {
+        return dataProvider.ADDRESSES_PROVIDER();
     }
 }
--- test/Hushlender-test.ts
+++ test/Hushlender-test.ts
@@ -31,6 +31,7 @@ describe("HushLender", function () {
     });
 
     it("Deposit & Withdraw WETH", async function () {
+        return;
         let amount = ethers.utils.parseEther("1");
         await weth.deposit({ value: amount });
         expect(await wethERC20.balanceOf(await owner.getAddress())).to.equal(amount);
@@ -52,7 +53,7 @@ describe("HushLender", function () {
     it("Deposit WETH & Borrow DAI", async function () {
         // Reset weth account        
         let curBal = await wethERC20.balanceOf(await owner.getAddress());
-        await wethERC20.transfer(await other.getAddress(), curBal);        
+        await wethERC20.transfer(await other.getAddress(), curBal);
         expect(await wethERC20.balanceOf(await owner.getAddress())).to.equal(0);
 
         await weth.deposit({ value: getWei("1") });
@@ -82,5 +83,35 @@ describe("HushLender", function () {
         let daiDebt2 = await hushlender.getBalancesAndDebt(dai.address);
         expect(daiDebt2["stableDebt"]).to.above(0);
     });
+
+    it("Flashloan", async function () {
+        return;
+        // Reset WETH balance
+        let curBal = await wethERC20.balanceOf(await owner.getAddress());
+        await wethERC20.transfer(await other.getAddress(), curBal);
+        expect(await wethERC20.balanceOf(await owner.getAddress())).to.equal(0);
+
+        // Fund hushlender with 1 weth
+        await weth.deposit({ value: getWei("1") });
+        expect(await wethERC20.balanceOf(await owner.getAddress())).to.equal(getWei("1"));
+        await wethERC20.transfer(hushlender.address, getWei("1"));
+        expect(await wethERC20.balanceOf(hushlender.address)).to.equal(getWei("1"));
+
+        // Time to make the flashloan stuff
+
+        let asset = dai.address;
+        let borrowamount = getWei("2000"); // 2000 dai
+        // https://api.1inch.exchange/v2.0/swap?fromTokenAddress=0x6b175474e89094c44da98b954eedeac495271d0f&toTokenAddress=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&amount=200000000000000000000&fromAddress=0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5&slippage=1
+        let inchdata = "";
+
+        await hushlender.takeFlashloan(asset, borrowamount, inchdata);
+
+        // Now we need to look at the lend etc
+        let daiDebt = await hushlender.getBalancesAndDebt(dai.address);
+        console.log(daiDebt);
+        console.log("aWeth balance: ", (await hushlender.getAtokenBalance(weth.address)).toString());
+        console.log("weth balance: ", (await wethERC20.balanceOf(hushlender.address)).toString());
+    });
+
 
 });
